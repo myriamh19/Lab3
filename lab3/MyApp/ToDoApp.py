23f5f80e1fdf43a39efffd5149aa187df25b55e4
myriamh19 <myriamh@umich.edu>
2024-09-26 20:38:20 +0000

    put a reasonable message

--- lab3/MyApp/ToDoApp.py
+++ lab3/MyApp/ToDoApp.py
@@ -5,7 +5,7 @@ class ToDoModel:
     def add_task(self, task):
         self.tasks.append(task) # Add a task to the list
 
-    # def delete_task(self, task_index):
+    def delete_task(self, task_index):
 # if 0 <= task_index < len(self.tasks):
 # del self.tasks[task_index] # Delete the task at the specified index
 
